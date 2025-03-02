abb8ee74ebb3674c60dd1cbad1a61492950f1adb
Ariel Etchart <ariel.limes@gmail.com>
2025-03-02 15:14:04 -0300

    Refactor earnings calculation to use iterators and add complex example with SupportStaff

--- iterators_6/src/main.rs
+++ iterators_6/src/main.rs
@@ -10,6 +10,24 @@ struct SupportStaff {
 
 fn main() {
     let earnings: [i32;4] = [4,7,10,15];
-    let sum = earnings.into_iter().fold(0, |acc, x| acc + x);
+    let sum = earnings.iter().fold(0, |acc, x| acc + x);
     println!("Total earnings: {}", sum);
+
+
+    // More Complex Example //////////////////////////////////////////////////////////
+    let week = [
+        SupportStaff {day: "Monday".to_string(), name: "John".to_string()},
+        SupportStaff {day: "Tuesday".to_string(), name: "Jane".to_string()},
+        SupportStaff {day: "Wednesday".to_string(), name: "Doe".to_string()},
+        SupportStaff {day: "Thursday".to_string(), name: "Smith".to_string()}
+    ];
+    
+    let staff = week.iter().fold(
+        HashMap::new(),
+        |mut acc,x| {
+            acc.insert(&x.day, &x.name);
+            acc
+        }
+    );
+    println!("{:?}", staff);
 }
